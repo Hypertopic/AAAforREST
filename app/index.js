Hypertopic/AAAforREST

commit 141cbf37ea9e0c34f012ab0e5bc8162ccf3d8157

FEATURE: User and date of last update is kept for accountability.
The prefix in the object can be changed in settings.
No prefix means no accountability.

Needed for Hypertopic/Porphyry#152, Hypertopic/Porphyry#501, 
Hypertopic/Porphyry#219.

app/index.js
@@ -19,6 +19,7 @@ module.exports = class AAAforREST {
    *  url: {string}
    *  searchBase: {string}
    *  searchFilter: {string}
+   * record: {string}
    */
   constructor(settings) {
     this.settings = settings;
@@ -35,6 +36,16 @@ module.exports = class AAAforREST {
         return req;
       }
     })
+  }
+
+  recordIfUpdate = (request, response, next) => {
+    if (this.settings.record && request.body && new RegExp('^/[^/]*$').test(request.url)) {
+      request.body[this.settings.record] = {
+        contributor: request.auth.name,
+        modified: new Date()
+      };
+    }
+    next();
   }
 
   continueIfContentType = (types) => (request, response, next) => {

app/proxy.js
@@ -69,6 +69,8 @@ app.route('*')
     aaa.checkAuthenticationOnHTTP,
     aaa.continueIfAuthentified,
     aaa.updateHeaders,
+    bodyParser.json(),
+    aaa.recordIfUpdate,
     aaa.forward({preserveCredentials: false})
   );
 

conf/config.yml
@@ -21,4 +21,5 @@ ldap:
   url: ldap://ldap.forumsys.com
   searchBase: dc=example,dc=com
   searchFilter: (uId={{username}})
+record: record
 

commit 538ba8110b2a513ee7a4f0196f814c2072561b33

FIX: Request body may be in other formats than JSON.
This regression, caused by 141cbf37ea9e0c34f012ab0e5bc8162ccf3d8157,
prevented pictures to be uploaded to Steatite.

app/index.js
@@ -4,6 +4,7 @@ const Ldap = require('./ldap');
 const fetch = require('node-fetch');
 const createHmac = require('crypto').createHmac;
 const session = require('express-session');
+const bodyParser = require('body-parser');
 
 /**
  * Every method (except the constructor) returns an Express middleware.
@@ -38,14 +39,23 @@ module.exports = class AAAforREST {
     })
   }
 
-  recordIfUpdate = (request, response, next) => {
-    if (this.settings.record && request.body && new RegExp('^/[^/]*$').test(request.url)) {
-      request.body[this.settings.record] = {
-        contributor: request.auth.name,
-        modified: new Date()
-      };
+  recordInBody = (request, response, next) => {
+    if (this.settings.record 
+      && request.headers['content-type'] === 'application/json'
+      && new RegExp('^/[^/]*$').test(request.url)
+    ) {
+      bodyParser.json({extended: false})(request, response, () => {
+        if (request.body) {
+          request.body[this.settings.record] = {
+            contributor: request.auth.name,
+            modified: new Date()
+          };
+        }
+        next();
+      });
+    } else {
+      next();
     }
-    next();
   }
 
   continueIfContentType = (types) => (request, response, next) => {

app/proxy.js
@@ -69,8 +69,7 @@ app.route('*')
     aaa.checkAuthenticationOnHTTP,
     aaa.continueIfAuthentified,
     aaa.updateHeaders,
-    bodyParser.json(),
-    aaa.recordIfUpdate,
+    aaa.recordInBody,
     aaa.forward({preserveCredentials: false})
   );
 